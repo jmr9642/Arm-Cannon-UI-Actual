26a423d523b32698ea25c0f08dcdcb29225cfeac
jmr9642 <jmr9642@g.rit.edu>
2018-03-26 01:33:09 -0400

    Menu / Highscore

---
+++ Arm Cannon UI/Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+//PLAYER SCORE DOES NOT STOP ON PLAYER DEATH. NEED ACCESS TO GITHUB PROJECT CODE TO IMPLEMENT.
+
+
+public class ScoreManager : MonoBehaviour {
+
+	public Text scoreText;
+	public Text highScoreText;
+
+	public float scoreCount;
+	public float highScoreCount;
+
+	public float pointsPerSecond;
+
+	public bool scoreIncreasing;
+
+	// Use this for initialization
+	void Start () {
+		if (PlayerPrefs.HasKey ("Highscore")) 
+		{
+			highScoreCount = PlayerPrefs.GetFloat ("Highscore");
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (scoreIncreasing) 
+		{
+			scoreCount += pointsPerSecond * Time.deltaTime;
+		}
+
+		if (scoreCount > highScoreCount) 
+		{
+			highScoreCount = scoreCount;
+			PlayerPrefs.SetFloat ("Highscore", highScoreCount);
+		}
+
+		scoreText.text = "Score: " + Mathf.Round (scoreCount);
+		highScoreText.text = "Highscore: " + Mathf.Round (highScoreCount);
+	}
+}
